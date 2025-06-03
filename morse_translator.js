e12f2ea0163427dec1d8d00f27acaac8987fcd8e
Sharleen10 <sharleensshabangu@gmail.com>
2025-06-03 09:48:46 +0200

    feat(audio): add SoundManager class to play Morse code audio beeps

--- morse_translator.js
+++ morse_translator.js
@@ -158,3 +158,67 @@
                 return this.decoder.decode(code);
             }
         }
+
+          /**
+         *  SoundManager - Handles audio playback
+         */
+        class SoundManager {
+            constructor() {
+                this.audioContext = null;
+                this.dotDuration = 0.1;
+                this.dashDuration = 0.3;
+                this.pauseDuration = 0.1;
+            }
+
+            initAudioContext() {
+                if (!this.audioContext) {
+                    this.audioContext = new (window.AudioContext || window.webkitAudioContext)();
+                }
+                return this.audioContext;
+            }
+
+            playMorseSound(textareaId) {
+                const morseText = document.getElementById(textareaId).value;
+                if (!morseText) {
+                    ui.showStatus('No morse code to play!', 'error');
+                    return;
+                }
+
+                ui.showStatus('🔊 R2-D2 is beeping your message...', 'success');
+                
+                const audioContext = this.initAudioContext();
+                let currentTime = audioContext.currentTime;
+                
+                for (let i = 0; i < morseText.length; i++) {
+                    const char = morseText[i];
+                    if (char === '.') {
+                        this.playBeep(audioContext, currentTime, 800, this.dotDuration);
+                        currentTime += this.dotDuration + this.pauseDuration;
+                    } else if (char === '-') {
+                        this.playBeep(audioContext, currentTime, 800, this.dashDuration);
+                        currentTime += this.dashDuration + this.pauseDuration;
+                    } else if (char === ' ') {
+                        currentTime += this.pauseDuration * 2;
+                    } else if (char === '/') {
+                        currentTime += this.pauseDuration * 4;
+                    }
+                }
+            }
+
+            playBeep(audioContext, startTime, frequency, duration) {
+                const oscillator = audioContext.createOscillator();
+                const gainNode = audioContext.createGain();
+                
+                oscillator.connect(gainNode);
+                gainNode.connect(audioContext.destination);
+                
+                oscillator.frequency.value = frequency;
+                oscillator.type = 'sine';
+                
+                gainNode.gain.setValueAtTime(0.3, startTime);
+                gainNode.gain.exponentialRampToValueAtTime(0.01, startTime + duration);
+                
+                oscillator.start(startTime);
+                oscillator.stop(startTime + duration);
+            }
+        }
